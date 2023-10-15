db5f487aa2cb7efcbe24cea2d42a7d7b7df3b932
Evan Pratten <evan@ewpratten.com>
2023-10-15 14:42:58 -0400

    soft fail if rss is bad

--- daily-digest/modules/rss/fetch.py
+++ daily-digest/modules/rss/fetch.py
@@ -7,29 +7,32 @@ from .article import Article
 
 logger = logging.getLogger(__name__)
 
+
 def get_articles_by_publisher(publisher: Publisher) -> List[Article]:
-    
     # Query the RSS feed
     logger.debug(f"Getting articles from {publisher.name}")
     response = requests.get(publisher.rss_url)
-    
+
     # If the request failed, raise an exception
     if response.status_code != 200:
-        raise Exception(
+        logger.error(
             f"Failed to get articles from {publisher.name}. Status code: {response.status_code}. Response: {response.text}"
         )
-        
+        return []
+
     # Parse the RSS response
     feed = feedparser.parse(response.text)
-    
+
     # Convert the feed entries into Article objects
     articles = []
     for entry in feed.entries:
-        articles.append(Article(
-            title=entry.title,
-            author=publisher.name,
-            url=entry.link,
-            category=publisher.category,
-        ))
-        
+        articles.append(
+            Article(
+                title=entry.title,
+                author=publisher.name,
+                url=entry.link,
+                category=publisher.category,
+            )
+        )
+
     return articles
